9d1da565f096edddbb2d728c40c75bd265d64d54
Rahyll <henry.mr6@gmail.com>
2023-07-31 17:04:14 +0530

    crud with update and delete

--- routes/contact.js
+++ routes/contact.js
@@ -38,6 +38,29 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.delete("/", async (req, res) => {
+  console.log(req.body._id);
+  const obj_id = new ObjectId(req.body._id);
 
+  try {
+    const deletedContact = await Contact.deleteOne({ _id: obj_id });
+    res.json(deletedContact);
+  } catch (err) {
+    res.send("Cannot Delete");
+  }
+});
+
+router.patch("/", async (req, res) => {
+  const { _id, userName, password } = req.body;
+  try {
+    const updatedContact = await Contact.updateOne(
+      { _id },
+      { $set: { userName, password } }
+    );
+    res.json(updatedContact);
+  } catch (err) {
+    res.send("could not update");
+  }
+});
 
 module.exports = router;
